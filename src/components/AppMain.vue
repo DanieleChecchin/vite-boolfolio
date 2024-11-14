34731bce4c2260d55d575721b3450d1f2747825a
DanieleChecchin <danielechecchin26@gmail.com>
2024-11-14 16:35:58 +0100

    added bootstrap and sass

---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getProjects() {
+
+        }
+    },
+}
+</script>
+
+<template>
+</template>
+
+<style scoped></style>
